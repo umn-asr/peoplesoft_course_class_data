4e5609ccfdba52dde1ca30fd3f8e5ffc6b6953da
Davin Lagerroos <dlagerro@umn.edu>
2015-03-10 14:59:36 -0500

    require new classes to qas.rb file
    update qas.rb with new files so all classes in the Qas namespace
    get loaded when a qas gets required

--- lib/qas/qas.rb
+++ lib/qas/qas.rb
@@ -1,3 +1,6 @@
+require_relative 'soap_request'
+require_relative 'credentials'
+require_relative 'soap_request_builder'
 require_relative 'query_with_polled_response'
 require_relative 'start_sync_poll_query'
 require_relative 'get_query_results'
