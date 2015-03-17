d6a6e3a9663a8a58bbb495e3c27f11c16955cc34
Davin Lagerroos <dlagerro@umn.edu>
2015-03-17 16:12:31 -0500

    add Resource as a super class for Section
    and verfiy it's extraction using the section spec

---
+++ lib/xml_parser/resource.rb
@@ -0,0 +1,61 @@
+module PeoplesoftCourseClassData
+  module XmlParser
+    class Resource
+      # ATTRIBUTES        = []
+      # CHILD_COLLECTIONS = []
+
+      def self.configure_attributes(attributes)
+        attributes.each do |attribute|
+          puts "adding attribute #{attribute}"
+          self.send(:attr_reader, attribute)
+          self.send(:attr_writer, attribute)
+          class_eval "private :#{attribute}="
+        end
+      # (ATTRIBUTES + CHILD_COLLECTIONS).each do |attribute|
+      #   puts "adding attribute #{attribute}"
+      #   self.send(:attr_reader, attribute)
+      #   self.send(:attr_writer, attribute)
+      #   class_eval "private :key => "value", #{attribute}="
+      end
+
+      # configure_attributes(ATTRIBUTES + CHILD_COLLECTIONS)
+
+      def initialize(*args)
+        self.class.attributes.each_with_index do |attribute, index|
+          self.send "#{attribute}=", args[index]
+        end
+        self.class.child_collections.each_with_index do |collection, index|
+          index_with_offset = self.class.attributes.count + index
+          collection_class = Object.const_get "PeoplesoftCourseClassData::XmlParser::#{collection.to_s.split('_').map(&:capitalize).join('')}"
+          self.send "#{collection}=", collection_class.new(args[index_with_offset])
+        end
+      end
+
+      def ==(other)
+        self.class.attributes.inject(true) do |result, attribute|
+          result && (self.send(attribute) == other.send(attribute))
+        end
+      end
+      alias_method :eql?, :==
+
+      def hash
+        self.class.attributes.map(&:hash).inject(:+)
+      end
+
+      def merge(other)
+        self.class.child_collections.each do |collection|
+          other_child      = other.send(collection).first
+          matching_child   = self.send(collection).detect { |item| item == other_child }
+          if matching_child
+            matching_child.merge(other_child)
+          else
+            self.send(collection).merge(other.send(collection))
+          end
+        end
+        self
+      end
+
+      private
+    end
+  end
+end
