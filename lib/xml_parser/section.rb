ba8e2621e6c23adc2b4c55a8b5a100d4affe6027
Davin Lagerroos <dlagerro@umn.edu>
2015-03-17 16:24:32 -0500

    rewrite Section to use extracted Resource

--- lib/xml_parser/section.rb
+++ lib/xml_parser/section.rb
@@ -8,46 +8,16 @@ require_relative 'resource'
 module PeoplesoftCourseClassData
   module XmlParser
     class Section < Resource
-      attr_reader :class_number, :number, :component, :instructors, :combined_sections
-
-      def initialize(class_number, number, component, instructors = [], combined_sections = [])
-        self.class_number       = class_number
-        self.number             = number
-        self.component          = component
-        self.instructors        = Instructors.new(instructors)
-        self.combined_sections  = CombinedSections.new(combined_sections)
+      def self.attributes
+        [:class_number, :number, :component]
       end
 
-      def merge(other)
-        child_collections.each do |collection|
-          other_child      = other.send(collection).first
-          matching_child   = self.send(collection).detect { |item| item == other_child }
-          if matching_child
-            matching_child.merge(other_child)
-          else
-            self.send(collection).merge(other.send(collection))
-          end
-        end
-        self
-      end
-
-      def ==(other)
-        (self.class_number == other.class_number) &&
-          (self.number == other.number) &&
-          (self.component == other.component)
-      end
-      alias_method :eql?, :==
-
-      def hash
-        (class_number.hash + number.hash + component.hash)
-      end
-
-      def child_collections
+      def self.child_collections
         [:instructors, :combined_sections]
       end
 
-      private
-      attr_writer :class_number, :number, :component, :instructors, :combined_sections
+      configure_attributes(attributes + child_collections)
+
     end
   end
 end
