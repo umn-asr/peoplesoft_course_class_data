9e5b7245106281ce2c8521f2dd400bb478783ff6
Davin Lagerroos <dlagerro@umn.edu>
2015-03-26 14:59:20 -0500

    add campus resource

---
+++ lib/xml_parser/campus.rb
@@ -0,0 +1,13 @@
+require_relative 'resource'
+
+module PeoplesoftCourseClassData
+  module XmlParser
+    class Campus < Resource
+      def self.attributes
+        [:campus_id, :abbreviation]
+      end
+
+      configure_attributes(attributes)
+    end
+  end
+end
