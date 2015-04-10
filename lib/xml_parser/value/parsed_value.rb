775a65d5506dcbcaaa1047cb778342c4459bea4c
Davin Lagerroos <dlagerro@umn.edu>
2015-04-10 11:51:16 -0500

    add other equal methods for performance
    simple delegator works via method missing. when course aspects get
    merged there are a lot of equality checks on a lot of values. define
    equality to improve performance.
    
    benchmark-ips results:
    
      eql?, ==, and hash:   818154.3 i/s
               just eql?:   765116.1 i/s - 1.07x slower

--- lib/xml_parser/value/parsed_value.rb
+++ lib/xml_parser/value/parsed_value.rb
@@ -4,12 +4,20 @@ module PeoplesoftCourseClassData
   module XmlParser
     module Value
       class ParsedValue < SimpleDelegator
+        def ==(other)
+          __getobj__.==(other)
+        end
+
         def eql?(other)
           if self.class == other.class
             __getobj__.==(other.__getobj__)
           else
             super
           end
+        end
+
+        def hash
+          __getobj__.hash
         end
       end
     end
