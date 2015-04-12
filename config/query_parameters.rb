7c4edd7f81f2beb533fd71dedade2b489c9b21a4
Davin Lagerroos <dlagerro@umn.edu>
2015-04-12 17:02:27 -0500

    tweak QUERY_PARAMETERS for maintainability
    instead of having one big hash of all the combinations, maintain a
    list of terms and campuses. If we need to exclude a term/campus
    combination this will seem dumb, but I doubt that will happen anytime
    soon and the long term plan is to get this list from the rails app

--- config/query_parameters.rb
+++ config/query_parameters.rb
@@ -1,31 +1,38 @@
 module PeoplesoftCourseClassData
   module Config
-    QUERY_PARAMETERS = [
-                        {
-                          institution: "UMNDL",
-                          campus: "UMNDL",
-                          term: 1149
-                        },
-                        {
-                          institution: "UMNMO",
-                          campus: "UMNMO",
-                          term: 1149
-                        },
-                        {
-                          institution: "UMNCR",
-                          campus: "UMNCR",
-                          term: 1149
-                        },
-                        {
-                          institution: "UMNTC",
-                          campus: "UMNTC",
-                          term: 1149
-                        },
-                        {
-                          institution: "UMNTC",
-                          campus: "UMNRO",
-                          term: 1149
-                        }
-                      ]
+    CAMPUSES = [
+                {
+                  institution: "UMNDL",
+                  campus: "UMNDL"
+                },
+                {
+                  institution: "UMNMO",
+                  campus: "UMNMO"
+                },
+                {
+                  institution: "UMNCR",
+                  campus: "UMNCR"
+                },
+                {
+                  institution: "UMNTC",
+                  campus: "UMNTC"
+                },
+                {
+                  institution: "UMNTC",
+                  campus: "UMNRO"
+                }
+              ]
+    TERMS = [
+      {term: 1149},
+      {term: 1153},
+      {term: 1155},
+      {term: 1159},
+      {term: 1163}
+    ]
+
+    QUERY_PARAMETERS = CAMPUSES.inject([]) do |array, campus|
+                          array << TERMS.map { |term| campus.merge(term) }
+                       end.flatten
+
   end
 end
