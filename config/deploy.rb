199827be708b0e099379951db550d85c4ec0fb2a
Davin Lagerroos <dlagerro@umn.edu>
2015-04-13 15:43:24 -0500

    first stab at cap deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,20 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'peoplesoft_course_class_data'
+set :repo_url, 'https://github.com/umn-asr/peoplesoft_course_class_data'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :linked_dirs, %w(log vendor/bundle)
+set :linked_files, %w{config/initializers/environment_variables.rb}
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
+after "deploy:updated", :link_shared_tmp_folder do
+  # link in the shared tmp folder
+  run "ln -nfs /swadm/tmp #{release_path}/tmp"
 end
+
+after "deploy:finished", :reset_daemon do
+  run "DAEMON_ENV=#{stage} bundle exec #{release_path}/bin/peoplesoft_course_class_data"
+end
+
