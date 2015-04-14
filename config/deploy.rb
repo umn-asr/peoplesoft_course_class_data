44b8bcadb63e5f7c54689d9eac3d097b0b51c8f6
Davin Lagerroos <dlagerro@umn.edu>
2015-04-14 09:42:43 -0500

    rename link to shared tmp folder
    let the app have a tmp folder, use a different name for the shared
    json folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,12 +6,12 @@ set :repo_url, 'https://github.com/umn-asr/peoplesoft_course_class_data'
 
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-set :linked_dirs, %w(log vendor/bundle)
-set :linked_files, %w{config/initializers/environment_variables.rb}
+set :linked_dirs, %w(tmp log vendor/bundle)
+set :linked_files, %w{config/credentials.rb}
 
 after "deploy:updated", :link_shared_tmp_folder do
   # link in the shared tmp folder
-  run "ln -nfs /swadm/tmp #{release_path}/tmp"
+  run "ln -nfs /swadm/tmp #{release_path}/json_tmp"
 end
 
 after "deploy:finished", :reset_daemon do
