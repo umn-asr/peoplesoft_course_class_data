8db53f1b0832a34a14a5998bba822e840e0552ba
Davin Lagerroos <dlagerro@umn.edu>
2015-04-30 11:25:35 -0500

    configure daemon to send email for exceptions
    now that execptions are beign raised from the rake tasks, configure
    daemon kit to send emails when they occur

--- libexec/peoplesoft_course_class_data-daemon.rb
+++ libexec/peoplesoft_course_class_data-daemon.rb
@@ -36,9 +36,20 @@ end
 #end
 #
 # Example error handling (NOTE: all exceptions in scheduled tasks are logged)
-#DaemonKit::Cron.handle_exception do |job, exception|
-#  DaemonKit.logger.error "Caught exception in job #{job.job_id}: '#{exception}'"
-#end
+DaemonKit::Cron.handle_exception do |job, exception|
+  mail         = ::Mail.new
+  mail.from    = 'asrweb@umn.edu'
+  mail.to      = 'ASR-WEB-ERRORS@lists.umn.edu'
+  mail.subject = "Peoplesoft Course Class Data [#{ENV['DAEMON_ENV']}] - Exception"
+
+  mail.body = <<-EOF
+    An exception occured, details below:
+    Type: #{exception.class.name}
+    Message: #{exception.message}
+    EOF
+
+  mail.deliver
+end
 
 DaemonKit::Cron.scheduler.cron "0 23 * * 0-6" do
   path = "#{PeoplesoftCourseClassData::Config::FILE_ROOT}"
