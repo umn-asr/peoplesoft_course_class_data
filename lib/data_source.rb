93d62329f210ccdaa6de034615c913e43f065be5
Davin Lagerroos <dlagerro@umn.edu>
2015-06-02 11:57:58 -0500

    implement DataSource.build
    creates a new instance of DataSource using the supplied arguments

--- lib/data_source.rb
+++ lib/data_source.rb
@@ -1,6 +1,16 @@
 module PeoplesoftCourseClassData
   class DataSource
-    def self.build(service, parameters, env)
+    def self.build(service, query_parameters, env)
+      new(service, query_parameters, env)
     end
+
+    def initialize(service, query_parameters, env)
+      self.service          = service
+      self.query_parameters = query_parameters
+      self.env              = env
+    end
+
+    private
+    attr_accessor :service, :query_parameters, :env
   end
 end
---
+++ spec/lib/data_source_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../../lib/data_source'
+
+RSpec.describe PeoplesoftCourseClassData::DataSource, :focus do
+  let(:env)     { :tst }
+  let(:query)   { PeoplesoftCourseClassData::Config::QUERY_PARAMETERS.sample }
+  let(:service) { Object }
+
+  describe ".build" do
+    it "creates a new DataSource" do
+      expect(described_class.build(service, query, env)).to be_instance_of(PeoplesoftCourseClassData::DataSource)
+    end
+  end
+
+  describe "#term" do
+
+  end
+end
