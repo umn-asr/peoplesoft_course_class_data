7fe728104df5440673a5b28bdda91a320d1b6f58
Debbie Gillespie <debbieg@umn.edu>
2015-06-03 16:21:18 -0500

    Implements the run method
    Flattens the course_aspects array and then calls the merge method on
    each element in the course_aspects array.

--- lib/merge_data.rb
+++ lib/merge_data.rb
@@ -2,8 +2,8 @@ require_relative 'workflow'
 
 module PeoplesoftCourseClassData
   class MergeData
-    def self.run
-
+    def self.run(course_aspects, orchestrator)
+      course_aspects.flatten.inject(:merge)
     end
   end
 end
