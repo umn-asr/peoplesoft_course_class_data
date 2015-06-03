11851320a437df62b6239c3cff7c3a3811d54060
Debbie Gillespie <debbieg@umn.edu>
2015-06-03 16:23:59 -0500

    Adds and_return method
    Need to indicate the return value of the merge method

--- spec/lib/merge_data_spec.rb
+++ spec/lib/merge_data_spec.rb
@@ -14,11 +14,10 @@ RSpec.describe PeoplesoftCourseClassData::MergeData, :focus do
       class_service_results = [class_course_aspect_1, class_course_aspect_2]
       course_service_results = [course_course_aspect_1]
 
-      expect(class_course_aspect_1).to receive(:merge).with(class_course_aspect_2)
-      expect(class_course_aspect_1).to receive(:merge).with(course_course_aspect_1)
+      expect(class_course_aspect_1).to receive(:merge).with(class_course_aspect_2).and_return(class_course_aspect_1)
+      expect(class_course_aspect_1).to receive(:merge).with(course_course_aspect_1).and_return(class_course_aspect_1)
 
       expect(orchestrator).to receive(:run_step).with(PeoplesoftCourseClassData::WriteData, class_course_aspect_1)
-
       described_class.run([class_service_results, course_service_results], orchestrator)
     end
   end
