73069dc1fe0e0dffa62d28ebe73bc96835abbdfc
Debbie Gillespie <debbieg@umn.edu>
2015-06-04 17:08:26 -0500

    Refactor the merge_data test
    Now that the data is sorted in a previous step, course_id is not
    significant and whether the data is from the course source or the class
    source is not important.  Not positive this is correct, but want to get
    this into github so we work can continue while I am out.

--- spec/lib/merge_data_spec.rb
+++ spec/lib/merge_data_spec.rb
@@ -7,18 +7,26 @@ RSpec.describe PeoplesoftCourseClassData::MergeData do
 
     it "merges course aspects with the same course_id together" do
       COURSE_ID_1 = 1
-      class_course_aspect_1 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
-      class_course_aspect_2 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
-      course_course_aspect_1 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
+      course_1_aspect_1 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
+      course_1_aspect_2 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
+      course_1_aspect_3 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
+      course_1_aspect_4 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
+      course_1_aspect_5 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
 
-      class_service_results = [class_course_aspect_1, class_course_aspect_2]
-      course_service_results = [course_course_aspect_1]
+      collection_of_course_aspects = [course_1_aspect_1, course_1_aspect_2, course_1_aspect_3]
+      another_collection_of_course_aspects = [course_1_aspect_4, course_1_aspect_5]
 
-      expect(class_course_aspect_1).to receive(:merge).with(class_course_aspect_2).and_return(class_course_aspect_1)
-      expect(class_course_aspect_1).to receive(:merge).with(course_course_aspect_1).and_return(class_course_aspect_1)
+      collection_of_course_aspects[1..-1].each do |ca|
+        expect(collection_of_course_aspects[0]).to receive(:merge).with(ca).and_return(collection_of_course_aspects[0])
+        described_class.run([collection_of_course_aspects[0], ca], orchestrator)
+      end
 
-      expect(orchestrator).to receive(:run_step).with(PeoplesoftCourseClassData::WriteData, class_course_aspect_1)
-      described_class.run([class_service_results, course_service_results], orchestrator)
+      another_collection_of_course_aspects[1..-1].each do |ca|
+        expect(another_collection_of_course_aspects[0]).to receive(:merge).with(ca).and_return(another_collection_of_course_aspects[0])
+        described_class.run([another_collection_of_course_aspects[0], ca], orchestrator)
+      end
+
+      expect(orchestrator).to receive(:run_step).with(PeoplesoftCourseClassData::WriteData, course_1_aspect_1)
     end
   end
 end
