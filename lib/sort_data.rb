52e02c4575d4fcde6bb7537dbc08d95dfbd5b858
Davin Lagerroos <dlagerro@umn.edu>
2015-06-04 15:48:58 -0500

    add SortData#run
    SortData#run takes collections of data from the parsers, groups
    the objects by course_id and hands them off the MergeData

--- lib/sort_data.rb
+++ lib/sort_data.rb
@@ -2,6 +2,9 @@ require_relative 'workflow'
 
 module PeoplesoftCourseClassData
   class SortData
-
+    def self.run(parsed_data, orchestrator)
+      sorted_data = parsed_data.flatten.to_set.classify { |course_aspect| course_aspect.course_id }.values
+      orchestrator.run_step(MergeData, sorted_data)
+    end
   end
 end
---
+++ spec/lib/sort_data_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../../lib/sort_data'
+
+RSpec.describe PeoplesoftCourseClassData::SortData do
+  describe "#run" do
+    it "returns collections of items with the same course_id" do
+      orchestrator = instance_double("PeoplesoftCourseClassData::ClassJsonExport")
+
+      course_ids = (1..rand(3..5)).to_a
+      class_data = 10.times.map { |i| PeoplesoftCourseClassData::XmlParser::CourseAspect.new(course_ids.sample, "class_catalog_#{i}") }
+      course_data = 10.times.map { |i| PeoplesoftCourseClassData::XmlParser::CourseAspect.new(course_ids.sample, "course_catalog_#{i}") }
+
+      classified_data = [class_data, course_data].flatten.to_set.classify { |course_aspect| course_aspect.course_id }
+      expected = classified_data.values
+
+      expect(orchestrator).to receive(:run_step).with(PeoplesoftCourseClassData::MergeData, expected)
+      described_class.run([class_data, course_data], orchestrator)
+    end
+  end
+end
