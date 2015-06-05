efe5fdff74eba20fe676d79aa6ef28a957e24982
Davin Lagerroos <dlagerro@umn.edu>
2015-06-05 09:23:44 -0500

    finish MergeData run test
    this test was in progress at the end of the day yesterday. finishing
    it up.
    
    Assert that each collection of course aspects gets merged into a
    single course aspect and that this collection of merged courses
    gets sent to WriteData

--- lib/merge_data.rb
+++ lib/merge_data.rb
@@ -2,8 +2,9 @@ require_relative 'workflow'
 
 module PeoplesoftCourseClassData
   class MergeData
-    def self.run(course_aspects, orchestrator)
-      course_aspects.flatten.inject(:merge)
+    def self.run(collections, orchestrator)
+      courses = collections.map { |collection| collection.inject(:merge) }
+      orchestrator.run_step(WriteData, courses)
     end
   end
 end
--- spec/lib/merge_data_spec.rb
+++ spec/lib/merge_data_spec.rb
@@ -1,32 +1,27 @@
 require_relative '../../lib/merge_data'
 require_relative '../../lib/xml_parser/course_aspect'
 
-RSpec.describe PeoplesoftCourseClassData::MergeData do
+RSpec.describe PeoplesoftCourseClassData::MergeData, :focus do
   describe "#run" do
     let(:orchestrator) { instance_double("PeoplesoftCourseClassData::ClassJsonExport") }
 
-    it "merges course aspects with the same course_id together" do
+    it "merges each collection of course aspects together" do
       COURSE_ID_1 = 1
-      course_1_aspect_1 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
-      course_1_aspect_2 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
-      course_1_aspect_3 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
-      course_1_aspect_4 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
-      course_1_aspect_5 = PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1)
+      COURSE_ID_2 = 2
 
-      collection_of_course_aspects = [course_1_aspect_1, course_1_aspect_2, course_1_aspect_3]
-      another_collection_of_course_aspects = [course_1_aspect_4, course_1_aspect_5]
+      collection_of_course_aspects = rand(5..10).times.map do { PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1) }
+      another_collection_of_course_aspects = rand(5..10).times.map { PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_2) }
 
-      collection_of_course_aspects[1..-1].each do |ca|
-        expect(collection_of_course_aspects[0]).to receive(:merge).with(ca).and_return(collection_of_course_aspects[0])
-        described_class.run([collection_of_course_aspects[0], ca], orchestrator)
+      collection_of_course_aspects[1..-1].each do |course_aspect|
+        expect(collection_of_course_aspects.first).to receive(:merge).with(course_aspect).and_return(collection_of_course_aspects.first)
       end
 
-      another_collection_of_course_aspects[1..-1].each do |ca|
-        expect(another_collection_of_course_aspects[0]).to receive(:merge).with(ca).and_return(another_collection_of_course_aspects[0])
-        described_class.run([another_collection_of_course_aspects[0], ca], orchestrator)
+      another_collection_of_course_aspects[1..-1].each do |course_aspect|
+        expect(another_collection_of_course_aspects.first).to receive(:merge).with(course_aspect).and_return(another_collection_of_course_aspects.first)
       end
 
-      expect(orchestrator).to receive(:run_step).with(PeoplesoftCourseClassData::WriteData, course_1_aspect_1)
+      expect(orchestrator).to receive(:run_step).with(PeoplesoftCourseClassData::WriteData, [collection_of_course_aspects.first, another_collection_of_course_aspects.first])
+      described_class.run([collection_of_course_aspects, another_collection_of_course_aspects], orchestrator)
     end
   end
 end
