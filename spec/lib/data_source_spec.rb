6724eb3460f4eb9642b75e9171ae70bbaff8d47a
Davin Lagerroos <dlagerro@umn.edu>
2015-06-06 11:45:43 -0500

    add tests for DataSource#data
    DataSource#data builds the data request for the DataSource's service
    and returns the results from querying the service with the institution,
    campus and term from the query config.
    
    Because the services can return data back in serveral responses, all
    responsed are gathered together and wrapped in a base tag so the responses
    look like a single xml document to parsers.

--- spec/lib/data_source_spec.rb
+++ spec/lib/data_source_spec.rb
@@ -1,12 +1,60 @@
 require_relative '../../lib/data_source'
 
 RSpec.describe PeoplesoftCourseClassData::DataSource do
-  let(:query_config)   { instance_double("PeoplesoftCourseClassData::QueryConfig") }
+  let(:query_config)   do
+                          instance_double(
+                                          "PeoplesoftCourseClassData::QueryConfig",
+                                          institution: 'UMNTC',
+                                          campus: 'UMNTC',
+                                          term: '1149',
+                                          env: :tst
+                                        )
+                      end
   let(:service) { Object }
 
   describe ".build" do
     it "creates a new DataSource" do
       expect(described_class.build(service, query_config)).to be_instance_of(PeoplesoftCourseClassData::DataSource)
+    end
+  end
+
+  describe "#data" do
+    let(:soap_request_instance) { instance_double("PeoplesoftCourseClassData::Qas::SoapRequest") }
+    let(:service)               { PeoplesoftCourseClassData::ClassService }
+    let(:service_instance)      { instance_double("PeoplesoftCourseClassData::ClassService") }
+
+    subject { described_class.new(service, query_config) }
+
+    before do
+      # stub out collaborators so we can make assertions about each step in seperate tests
+      allow(PeoplesoftCourseClassData::Qas::SoapRequestBuilder).to receive(:build).and_return(soap_request_instance)
+      allow(service).to receive(:new).and_return(service_instance)
+      allow(service_instance).to receive(:query).and_yield('')
+    end
+
+    it "configures a soap request for the supplied environment" do
+      expect(PeoplesoftCourseClassData::Qas::SoapRequestBuilder).to receive(:build).with(query_config.env).and_return(soap_request_instance)
+      subject.data
+    end
+
+    it "builds an instane of the service using the soap request" do
+      expect(service).to receive(:new).with(soap_request_instance).and_return(service_instance)
+      subject.data
+    end
+
+    it "returns the data from the service, wrapped in a tag" do
+      query_data = "<xml>data</xml>"
+      expected   = "<service_data>#{query_data}</service_data>"
+      allow(service_instance).to receive(:query).with(query_config.institution, query_config.campus, query_config.term).and_yield(query_data)
+      expect(subject.data).to eq(expected)
+    end
+
+    it "wraps all responses from the query in a single tag" do
+      response_1 = "<xml>response_1</xml>"
+      response_2 = "<xml>response_2</xml>"
+      expected   = "<service_data>#{response_1}#{response_2}</service_data>"
+      allow(service_instance).to receive(:query).with(query_config.institution, query_config.campus, query_config.term).and_yield(response_1).and_yield(response_2)
+      expect(subject.data).to eq(expected)
     end
   end
 
