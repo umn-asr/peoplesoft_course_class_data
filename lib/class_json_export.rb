c28c16a6496f7634d4d80e2b9fd6affa12415118
Davin Lagerroos <dlagerro@umn.edu>
2015-06-08 15:53:50 -0500

    remove unused code in ClassJsonExport
    our implementation has changed and we are no longer using this
    code

--- lib/class_json_export.rb
+++ lib/class_json_export.rb
@@ -33,44 +33,5 @@ module PeoplesoftCourseClassData
 
     private
     attr_accessor :env, :queries, :path
-
-    def download_xml(query)
-      File.open(xml_file(query), 'w+') do |f|
-        f.write("<class_service_data>")
-        class_service.query(query[:institution], query[:campus], query[:term]) do |response|
-          f.write(response)
-        end
-        f.write("</class_service_data>")
-      end
-
-      File.open(xml_file(query, "courses"), 'w+') do |f|
-        f.write("<course_service_data>")
-        course_service.query(query[:institution], query[:campus], query[:term]) do |response|
-          f.write(response)
-        end
-        f.write("</course_service_data>")
-      end
-    end
-
-    def convert_to_json(query)
-      PeoplesoftCourseClassData::XmlParser::ClassJson.new(xml_file(query)).to_file
-    end
-
-    def xml_file(query, service = "classes")
-      config = PeoplesoftCourseClassData::FileNameConfig.new(env, query, service, path)
-      PeoplesoftCourseClassData::FileNames.new(config).xml_with_path
-    end
-
-    def class_service
-      @class_service ||= PeoplesoftCourseClassData::ClassService.new(soap_request)
-    end
-
-    def course_service
-      @course_service ||= PeoplesoftCourseClassData::CourseService.new(soap_request)
-    end
-
-    def soap_request
-     @soap_request ||= PeoplesoftCourseClassData::Qas::SoapRequestBuilder.build(env)
-    end
   end
 end
