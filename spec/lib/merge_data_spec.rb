fbfe9efeb8288fcd8db5658447ed58d01167e84e
Debbie Gillespie <debbieg@umn.edu>
2015-06-08 17:09:24 -0500

    Update parameters passed to hash
    CourseAspect.new now expects the parameters to be passed as a hash
    instead of direct values.  Removed the constants since they are no
    longer needed - the hash keys explain what the numbers mean.

--- spec/lib/merge_data_spec.rb
+++ spec/lib/merge_data_spec.rb
@@ -6,11 +6,9 @@ RSpec.describe PeoplesoftCourseClassData::MergeData do
     let(:orchestrator) { instance_double("PeoplesoftCourseClassData::QueryResults") }
 
     it "merges each collection of course aspects together" do
-      COURSE_ID_1 = 1
-      COURSE_ID_2 = 2
 
-      collection_of_course_aspects = rand(5..10).times.map { PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_1) }
-      another_collection_of_course_aspects = rand(5..10).times.map { PeoplesoftCourseClassData::XmlParser::CourseAspect.new(COURSE_ID_2) }
+      collection_of_course_aspects = rand(5..10).times.map { PeoplesoftCourseClassData::XmlParser::CourseAspect.new(course_id: 1) }
+      another_collection_of_course_aspects = rand(5..10).times.map { PeoplesoftCourseClassData::XmlParser::CourseAspect.new(course_id: 2) }
 
       collection_of_course_aspects[1..-1].each do |course_aspect|
         expect(collection_of_course_aspects.first).to receive(:merge).with(course_aspect).and_return(collection_of_course_aspects.first)
