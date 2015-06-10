995e9a2a6f8309bd68e5473c15a450654b2e6760
Davin Lagerroos <dlagerro@umn.edu>
2015-06-10 12:49:22 -0500

    add integration-like test for xml to json conversion
    Add a test to make sure our xml conversion gives us the json we expect.
    The test is not a full end-to-end integration test because we are
    stubbing out the responses from the peoplesoft services, but does make
    sure the rest of the system integrates

---
+++ spec/class_json_export_with_reference_data_spec.rb
@@ -0,0 +1,39 @@
+require_relative '../lib/class_json_export'
+
+RSpec.describe 'XmlToJsonIntegration', :focus do
+  describe "ClassJsonExport#run with actual xml data" do
+    it "matches the reference json" do
+      env        = :prd
+      parameters = {
+                      institution: "UMNTC",
+                      campus: "UMNTC",
+                      term: "1149"
+                    }
+      path       = File.join(PeoplesoftCourseClassData::Config::FILE_ROOT, 'spec/tmp')
+
+
+      class_xml_file = File.join(PeoplesoftCourseClassData::Config::FILE_ROOT, 'spec/fixtures/reference_classes.xml')
+      class_service_double = instance_double("PeoplesoftCourseClassData::ClassService")
+      allow(class_service_double).to receive(:query).and_yield(File.read(class_xml_file))
+
+      course_xml_file = File.join(PeoplesoftCourseClassData::Config::FILE_ROOT, 'spec/fixtures/reference_courses.xml')
+      course_service_double = instance_double("PeoplesoftCourseClassData::CourseService")
+      allow(course_service_double).to receive(:query).and_yield(File.read(course_xml_file  ))
+
+
+      allow(PeoplesoftCourseClassData::ClassService).to receive(:new).and_return(class_service_double)
+      allow(PeoplesoftCourseClassData::CourseService).to receive(:new).and_return(course_service_double)
+
+      reference_json_file = File.join(PeoplesoftCourseClassData::Config::FILE_ROOT, 'spec/fixtures/reference_combined.json')
+      actual_json_file = PeoplesoftCourseClassData::FileNames.new(PeoplesoftCourseClassData::QueryConfig.new(env, parameters), path).json_with_path
+
+      PeoplesoftCourseClassData::ClassJsonExport.new(env, path, [parameters]).run
+
+      reference_json = JSON.parse(File.read(reference_json_file), object_class: OpenStruct, array_class: Set)
+      actual_json = JSON.parse(File.read(actual_json_file), object_class: OpenStruct, array_class: Set)
+      expect(actual_json).to eq(reference_json)
+
+      `rm -f #{actual_json_file}`
+    end
+  end
+end
