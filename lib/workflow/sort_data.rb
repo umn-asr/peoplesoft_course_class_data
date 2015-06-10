67b5d28e321e5fc1ae6aeefd0376408aa69f8735
Davin Lagerroos <dlagerro@umn.edu>
2015-06-10 14:18:56 -0500

    change SortData to pass new test
    old implementation was using Set, which stopped working as expected
    when CourseAspect equality was changed

--- lib/workflow/sort_data.rb
+++ lib/workflow/sort_data.rb
@@ -1,7 +1,13 @@
 module PeoplesoftCourseClassData
   class SortData
     def self.run(parsed_data, orchestrator)
-      sorted_data = parsed_data.flatten.to_set.classify { |course_aspect| course_aspect.course_id }.values
+      all_course_aspects  = parsed_data.flatten
+      course_ids          = all_course_aspects.map(&:course_id).uniq.sort
+      puts "course_ids: #{course_ids}"
+      sorted_data         = course_ids.map do |course_id|
+                              all_course_aspects.select { |course_aspect| course_aspect.course_id == course_id }
+                            end
+      puts "sorted_data: #{sorted_data.count}"
       orchestrator.run_step(MergeData, sorted_data)
     end
   end
